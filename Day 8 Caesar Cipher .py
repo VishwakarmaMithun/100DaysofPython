1fa1cd62dfb8765e2ef1854a4014eb9031cab8a9
VishwakarmaMithun <mitunv@live.com>
2024-01-14 19:57:52 +0530

    100daysofPython

---
+++ Day 8 Caesar Cipher .py
@@ -0,0 +1,13 @@
+def greet():
+    print("Hello")
+    print("how do you do ?")
+    print("Isn't the weather is  nice today")
+
+greet()
+
+
+def greet_with_name(name):
+    print(f"Hello {name}")
+    print(f"Hoe do you do {name}?")
+
+greet_with_name("Mithun")
